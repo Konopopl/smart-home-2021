173cfd9920468ee207b921d1d3a6e7d369cb957f
Konopopl <55564270+Konopopl@users.noreply.github.com>
2021-03-17 23:22:45 +0300

    Add files via upload

---
+++ hometask_2/src/main/java/ru/sbt/mipt/oop/Application.java
@@ -0,0 +1,44 @@
+package ru.sbt.mipt.oop;
+
+import java.util.Arrays;
+
+import ru.sbt.mipt.oop.eventhandler.DoorEventHandler;
+import ru.sbt.mipt.oop.eventhandler.HallDoorEventHandler;
+import ru.sbt.mipt.oop.eventhandler.LightEventHandler;
+import ru.sbt.mipt.oop.eventprovider.RandomSensorEventProvider;
+import ru.sbt.mipt.oop.loader.SmartHomeJsonFileLoader;
+import ru.sbt.mipt.oop.loader.SmartHomeLoader;
+
+public class Application {
+    private final SmartHomeLoader smartHomeLoader;
+    private final EventCycleRunner eventCycleRunner;
+
+    public Application(SmartHomeLoader smartHomeLoader, EventCycleRunner eventCycleRunner) {
+        this.smartHomeLoader = smartHomeLoader;
+        this.eventCycleRunner = eventCycleRunner;
+    }
+
+
+    public static void main(String... args) {
+        new Application(
+                new SmartHomeJsonFileLoader("smart-home-1.js"),
+                new EventCycleRunner(
+                        new RandomSensorEventProvider(),
+                        Arrays.asList(
+                                new LightEventHandler(),
+                                new DoorEventHandler(),
+                                new HallDoorEventHandler()
+                        )
+                )
+        ).run();
+    }
+
+    public void run() {
+        // считываем состояние дома из файла
+        SmartHome smartHome = smartHomeLoader.loadSmartHome();
+
+        // начинаем цикл обработки событий
+        eventCycleRunner.run(smartHome);
+    }
+
+}
---
+++ hometask_2/src/main/java/ru/sbt/mipt/oop/SmartHome.java
@@ -0,0 +1,29 @@
+package ru.sbt.mipt.oop;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import ru.sbt.mipt.oop.component.Room;
+
+public class SmartHome implements Actionable {
+    Collection<Room> rooms;
+
+    public SmartHome() {
+        rooms = new ArrayList<>();
+    }
+
+    public SmartHome(Collection<Room> rooms) {
+        this.rooms = rooms;
+    }
+
+    public void addRoom(Room room) {
+        rooms.add(room);
+    }
+
+    @Override
+    public void execute(Action action) {
+        action.accept(this);
+
+        rooms.forEach(room -> room.execute(action));
+    }
+}
